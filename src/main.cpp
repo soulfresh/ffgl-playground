2d83d6ad5ab6da6786f9847c4d8a6bb4209b70e1
Marc Wren <things@soul-fresh.com>
2022-02-01 22:22:07 -0500

    Added FFGL as a submodule dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 // Your First C++ Program
 
 #include <iostream>
+#include <glm/glm.hpp>
 
 int main() {
-	std::cout << "Hello World!";
+	glm::vec3 foo = {10, 20, 30};
+	std::cout << "Hello World!" << foo.x << foo.y << foo.z;
 	return 0;
 }
 
