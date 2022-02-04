be588b03497c2e7defe17ea2c37baac22c8e6006
Marc Wren <things@soul-fresh.com>
2022-02-03 21:30:51 -0500

    Updated the project to pull GLM library as a submodule.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # lolpxl
 FFGL plugins
+
+# Setup
+
+    git clone --recursive git@github.com:soulfresh/lolpxl.git
+
+# Adding New Libraries
+
+    cd ./deps
+    git submodule add ${REPO_URL}
+    cd ./${REPO_NAME}
+    git checkout tags/${VERSION_NUMBER}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 // Your First C++ Program
 
 #include <iostream>
-#include <glm/glm.hpp>
+#include <glm.hpp>
+#include "FFGLSDK.h"
 
 int main() {
 	glm::vec3 foo = {10, 20, 30};
-	std::cout << "Hello World!" << foo.x << foo.y << foo.z;
+	std::cout << "Vector: (" << foo.x << "," << foo.y << "," << foo.z << ")";
+	int x;
+	std::cin >> x;
 	return 0;
 }
 
